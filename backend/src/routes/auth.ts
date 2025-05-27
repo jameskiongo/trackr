cae9dd368454600fc49ba4cbea4426061079f860
jameskiongo <kiongojames2000@gmail.com>
2025-05-27 11:10:22 +0300

    fix: adding types for login

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { TLoginRequest, TLoginResponse } from "../types";
 
 const router = express.Router();
 
@@ -6,8 +7,18 @@ router.get("/login", (_req, res) => {
   res.send("Login Page");
 });
 
-router.post("/login", (_req, res) => {
-  res.send("Login Page");
-});
+router.post(
+  "/login",
+  (
+    req: Request<unknown, unknown, TLoginRequest>,
+    res: Response<TLoginResponse>,
+  ) => {
+    const { email, password }: TLoginRequest = req.body;
+    console.log({ email, password });
+    const name = "james";
+    const id = 2;
+    res.send({ email, id, name });
+  },
+);
 
 export default router;
---
+++ backend/src/types.ts
@@ -0,0 +1,9 @@
+export interface TLoginRequest {
+  email: string;
+  password: string;
+}
+export interface TLoginResponse {
+  id: number;
+  email: string;
+  name: string;
+}
