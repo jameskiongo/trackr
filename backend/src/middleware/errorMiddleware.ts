7a4da7c700d25f063b9e87fbd003438904f11d9c
jameskiongo <kiongojames2000@gmail.com>
2025-06-15 21:16:11 +0300

    fix: changed linter and formatter

--- backend/src/middleware/errorMiddleware.ts
+++ backend/src/middleware/errorMiddleware.ts
@@ -1,47 +1,47 @@
-import { Request, Response, NextFunction } from "express";
+import type { NextFunction, Request, Response } from "express";
 import { z } from "zod";
 import { NewUserSchema } from "../utils";
 
 const LoginUserSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(6, "Password must be at least 6 characters long"),
+	email: z.string().email(),
+	password: z.string().min(6, "Password must be at least 6 characters long"),
 });
 
 export const newUserParser = (
-  req: Request,
-  _res: Response,
-  next: NextFunction,
+	req: Request,
+	_res: Response,
+	next: NextFunction,
 ) => {
-  try {
-    NewUserSchema.parse(req.body);
-    next();
-  } catch (error: unknown) {
-    next(error);
-  }
+	try {
+		NewUserSchema.parse(req.body);
+		next();
+	} catch (error: unknown) {
+		next(error);
+	}
 };
 
 export const loginUserParser = (
-  req: Request,
-  _res: Response,
-  next: NextFunction,
+	req: Request,
+	_res: Response,
+	next: NextFunction,
 ) => {
-  try {
-    LoginUserSchema.parse(req.body);
-    next();
-  } catch (error: unknown) {
-    next(error);
-  }
+	try {
+		LoginUserSchema.parse(req.body);
+		next();
+	} catch (error: unknown) {
+		next(error);
+	}
 };
 
 export const errorMiddleware = (
-  error: unknown,
-  _req: Request,
-  res: Response,
-  next: NextFunction,
+	error: unknown,
+	_req: Request,
+	res: Response,
+	next: NextFunction,
 ) => {
-  if (error instanceof z.ZodError) {
-    res.status(400).send({ error: error.issues });
-  } else {
-    next(error);
-  }
+	if (error instanceof z.ZodError) {
+		res.status(400).send({ error: error.issues });
+	} else {
+		next(error);
+	}
 };
