0e877eebd1f97fb4d7123cf9e19e0e77d0046b05
jameskiongo <kiongojames2000@gmail.com>
2025-06-15 21:11:53 +0300

    fix: changed linter and formatter (eslint->biome)auth and jobs route

--- backend/src/routes/jobs.ts
+++ backend/src/routes/jobs.ts
@@ -1,63 +1,86 @@
-import express, { Response, Request } from "express";
+import express, { type Response, type Request } from "express";
 import jwt from "jsonwebtoken";
 import { newJobParser, verifyToken } from "../middleware/authMiddleware";
 import { errorMiddleware } from "../middleware/errorMiddleware";
 import { getJob, getJobs } from "../services/jobs";
-import addJob, { AddJob } from "../services/jobs/addJobs";
+import addJob, { type AddJob } from "../services/jobs/addJobs";
 const router = express.Router();
 
 router.get("/", verifyToken, (req: Request, res: Response) => {
-  const token = req.headers.authorization?.split(" ")[1];
-  const decoded = jwt.verify(token!, process.env.JWT_SECRET_KEY!);
-  const userId = (decoded as { userId: number }).userId;
-  const jobs = getJobs(userId);
-  jobs
-    .then((jobs) => {
-      res.status(200).json(jobs);
-    })
-    .catch((error) => {
-      res.status(400).json("" + error);
-    });
+	const token = req.headers.authorization?.split(" ")[1];
+	if (!token) {
+		throw new Error("Token not provided");
+	}
+	const SecretKey = String(process.env.JWT_SECRET_KEY);
+	if (!process.env.JWT_SECRET_KEY) {
+		throw new Error("JWT_SECRET_KEY is not defined");
+	}
+	const decoded = jwt.verify(token, SecretKey);
+
+	const userId = (decoded as { userId: number }).userId;
+	const jobs = getJobs(userId);
+	jobs
+		.then((jobs) => {
+			res.status(200).json(jobs);
+		})
+		.catch((error) => {
+			res.status(400).json(`${error}`);
+		});
 });
 router.get(
-  "/:id",
-  verifyToken,
-  (req: Request<{ id: string }, unknown, unknown>, res: Response) => {
-    const paramId = Number(req.params.id);
-    const token = req.headers.authorization?.split(" ")[1];
-    const decoded = jwt.verify(token!, process.env.JWT_SECRET_KEY!);
-    const userId = (decoded as { userId: number }).userId;
-    const jobs = getJob({
-      userId: userId,
-      paramId: paramId,
-    });
-    jobs
-      .then((job) => {
-        res.status(200).json(job);
-      })
-      .catch((error) => {
-        res.status(400).json("" + error);
-      });
-  },
+	"/:id",
+	verifyToken,
+	(req: Request<{ id: string }, unknown, unknown>, res: Response) => {
+		const paramId = Number(req.params.id);
+		const token = req.headers.authorization?.split(" ")[1];
+		if (!token) {
+			throw new Error("Token not provided");
+		}
+		const SecretKey = String(process.env.JWT_SECRET_KEY);
+		if (!process.env.JWT_SECRET_KEY) {
+			throw new Error("JWT_SECRET_KEY is not defined");
+		}
+		const decoded = jwt.verify(token, SecretKey);
+
+		const userId = (decoded as { userId: number }).userId;
+		const jobs = getJob({
+			userId: userId,
+			paramId: paramId,
+		});
+		jobs
+			.then((job) => {
+				res.status(200).json(job);
+			})
+			.catch((error) => {
+				res.status(400).json(`${error}`);
+			});
+	},
 );
 router.post(
-  "/:id",
-  verifyToken,
-  newJobParser,
-  (req: Request<{ id: string }, unknown, AddJob>, res: Response) => {
-    const paramId = Number(req.params.id);
-    const token = req.headers.authorization?.split(" ")[1];
-    const decoded = jwt.verify(token!, process.env.JWT_SECRET_KEY!);
-    const userId = (decoded as { userId: number }).userId;
-    const response = addJob(req.body, userId, paramId);
-    response
-      .then(() => {
-        res.status(201).json("Success");
-      })
-      .catch((error) => {
-        res.status(400).json("" + error);
-      });
-  },
+	"/:id",
+	verifyToken,
+	newJobParser,
+	(req: Request<{ id: string }, unknown, AddJob>, res: Response) => {
+		const paramId = Number(req.params.id);
+		const token = req.headers.authorization?.split(" ")[1];
+		if (!token) {
+			throw new Error("Token not provided");
+		}
+		const SecretKey = String(process.env.JWT_SECRET_KEY);
+		if (!process.env.JWT_SECRET_KEY) {
+			throw new Error("JWT_SECRET_KEY is not defined");
+		}
+		const decoded = jwt.verify(token, SecretKey);
+		const userId = (decoded as { userId: number }).userId;
+		const response = addJob(req.body, userId, paramId);
+		response
+			.then(() => {
+				res.status(201).json("Success");
+			})
+			.catch((error) => {
+				res.status(400).json(`${error}`);
+			});
+	},
 );
 
 router.use(errorMiddleware);
