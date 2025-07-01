eb3e5b7c02a7b427035422a4b09caca19daf16c0
jameskiongo <kiongojames2000@gmail.com>
2025-07-01 12:47:07 +0300

    fix: reformating types file

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -1,47 +1,47 @@
-import { z } from "zod";
-import { NewUserSchema } from "./utils";
+import type { z } from "zod";
+import type { NewUserSchema } from "./utils";
 
 export interface TLoginRequest {
-  email: string;
-  password: string;
+	email: string;
+	password: string;
 }
 export interface TLoginResponse {
-  id: number;
-  email: string;
-  name: string;
-  token: string;
+	id: number;
+	email: string;
+	name: string;
+	token: string;
 }
 export type TRegisterRequest = z.infer<typeof NewUserSchema>;
 export interface TRegisterResponse {
-  id: number;
-  name: string;
-  email: string;
+	id: number;
+	name: string;
+	email: string;
 }
 export interface TDirectoryRequest {
-  name: string;
-  userId: number;
+	name: string;
+	userId: number;
 }
 export interface TDirectoryResponse {
-  id: number;
-  name: string;
+	id: number;
+	name: string;
 }
 export interface GetDirectoryResponse {
-  id: number;
-  name: string;
-  createdAt: string;
-  updatedAt: string;
+	id: number;
+	name: string;
+	createdAt: string;
+	updatedAt: string;
 }
 export interface EditDirectoryRequest {
-  userId: number;
-  name: string;
-  id: string;
+	userId: number;
+	name: string;
+	id: string;
 }
 export interface AddJobRequest {
-  userId: number;
-  company_name: string;
-  position_name: string;
-  status: string;
-  description: string;
-  application_url: string;
-  location: string;
+	userId: number;
+	company_name: string;
+	position_name: string;
+	status: string;
+	description: string;
+	application_url: string;
+	location: string;
 }
